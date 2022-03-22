70a4c95f2f8d81160979486d910872bc864f4bb0
AfonsoMeireles <afonso.msilva17@gmail.com>
2022-03-21 22:46:22 -0300

    ALTERACAO

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loader',
   template: `<div class="backdrop" *ngIf="isLoading">
-    <lottie-player class="loader"  src="../../../assets/v3.json" 
+    <lottie-player class="loader"  src="../../../assets/v4.json" 
      background="transparent"  speed="2" loop  autoplay >
   </lottie-player> 
   <!-- <img class="loader" src="https://assets.blu365.com.br/uploads/sites/4/2019/01/consultora-jequiti-cadastro.jpg" /> -->
